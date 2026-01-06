aefab9136f0ae8169d69592ef6312061abcbc3de
Abhijit <abhijitbanerjee2013@gmail.com>
2026-01-06 12:12:22 +0530

    Second commit

--- src/main/java/in/sp/main/App.java
+++ src/main/java/in/sp/main/App.java
@@ -14,7 +14,8 @@ public class App
     public static void main( String[] args )
     {
     	String config_loc="/in/sp/resources/applicationContext.xml";
-        ApplicationContext context=new ClassPathXmlApplicationContext(config_loc);
+        @SuppressWarnings("resource")
+		ApplicationContext context=new ClassPathXmlApplicationContext(config_loc);
         Student std=(Student)context.getBean("stdId");
         std.display();
         
