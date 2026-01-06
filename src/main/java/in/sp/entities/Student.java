8de1d8cf6841bde6a966e69fdce07ccc403ae48f
Abhijit <abhijitbanerjee2013@gmail.com>
2026-01-06 12:01:34 +0530

    first commit

---
+++ src/main/java/in/sp/entities/Student.java
@@ -0,0 +1,33 @@
+package in.sp.entities;
+
+public class Student {
+	private int rollNumber;
+	private String name;
+	private float marks;
+	
+	public int getRollNumber() {
+		return rollNumber;
+	}
+	public void setRollNumber(int rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getMarks() {
+		return marks;
+	}
+	public void setMarks(float marks) {
+		this.marks = marks;
+	}
+	
+	public void display() {
+		System.out.println("name : "+name);
+		System.out.println("Roll Number : "+rollNumber);
+		System.out.println("marks : "+marks);
+	}
+
+}
---
+++ src/main/java/in/sp/main/App.java
@@ -0,0 +1,22 @@
+package in.sp.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.sp.entities.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String config_loc="/in/sp/resources/applicationContext.xml";
+        ApplicationContext context=new ClassPathXmlApplicationContext(config_loc);
+        Student std=(Student)context.getBean("stdId");
+        std.display();
+        
+    }
+}
